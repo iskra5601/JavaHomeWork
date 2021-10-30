b2bf07fd308128f0a9a796d8f4aca530c0b1abb3
Dmytro Iskra <dmitrij.iskra@privatbank.ua>
2021-10-30 14:08:09 +0300

    init commit

---
+++ src/com/pb/iskra/hw4/Anagram.java
@@ -0,0 +1,47 @@
+package com.pb.iskra.hw4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите фразу:");
+        String comparison1 = scan.nextLine();
+        System.out.println("Введите фразу для сравнение :");
+        String comparison2 = scan.nextLine();
+        comparison1 = comparison1.replaceAll("[,.-]", "");
+
+
+        String comparison3 = comparison1.toUpperCase();
+        String comparison4 = comparison2.toUpperCase();
+
+
+        char[] sp = comparison3.toCharArray();
+        char[] sp1 = comparison4.toCharArray();
+        Arrays.sort(sp);
+        Arrays.sort(sp1);
+
+        if (anagram_checker(sp, sp1)) {
+            System.out.println("Да, это анаграмма.");
+        } else {
+            System.out.println("Нет. Это не аннаграмма");
+        }
+
+    }
+
+    public static boolean anagram_checker(char[] sp, char[] sp1) {
+        for (int i = 0; i < sp.length; i++) {
+
+            boolean isLetter = Character.isLetter(i);
+            if (isLetter == false) continue;
+
+            if (sp[i] != sp1[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
